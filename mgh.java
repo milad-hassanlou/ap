81afdd240bfef2fcb0d1e8ce7d6f5ffdd987a9c5
Milad <miladhassanlou100@gmail.com>
2025-03-02 10:55:38 +0330

    mgh calculous

---
+++ mgh.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class mgh {
+    public static void main(String[] args) {
+        final double g = 9.81;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(" math (kg): ");
+        double mass = scanner.nextDouble();
+
+
+        System.out.print(" hight (m): ");
+        double height = scanner.nextDouble();
+
+
+        double energy = mass * g * height;
+
+
+        System.out.println("W:" + energy);
+
+        scanner.close();
+    }
+}
+
+
+
