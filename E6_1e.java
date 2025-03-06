b359c02992c17991bdebebfc70c6523c43635c7b
MiladHassanlou <miladhassanlou1@gmail.com>
2025-03-06 11:52:06 +0330

    Practice (E6.1 e) Summation of odd digits of a number

---
+++ E6_1e.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class E6_1e {
+    public static void main(String[] args) {
+        System.out.print("Please input a number:");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int originalNum = num;
+        int sum = 0;
+        int r;
+        while (num > 0) {
+            r = num % 10;
+            if (r % 2 == 1) {
+                sum += r;
+            }
+            num /= 10;
+        }
+        System.out.printf("Summation of odd digits of %d is %d.", originalNum, sum);
+    }
+}
