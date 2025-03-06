e30c68614eeec479e78c8262402b71df4415d707
MiladHassanlou <miladhassanlou1@gmail.com>
2025-03-06 22:33:49 +0330

    Practice E6.8 Print Characters of a word seperated in lines.

---
+++ E6_8.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class E6_8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please input a word:");
+        String input = in.next();
+        System.out.println("Characters: ");
+
+        // First Way:
+        for (int i = 0; i < input.length(); i++) {
+            System.out.println(input.charAt(i));
+        }
+        /*
+
+        //Second Way:
+        char[] ch=input.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            System.out.println(ch[i]);
+        }
+
+        //third Way:
+        String[] chBlock = input.split("");
+        for(int i=0;i<chBlock.length;i++){
+        System.out.println(chBlock[i]);
+        }
+
+        */
+    }
+}
