ed05a975181e33eb4f284a809f34af2009415737
MiladHassanlou <miladhassanlou1@gmail.com>
2025-03-06 21:51:44 +0330

    Practice E6.5 operations on floating point numbers.

---
+++ E6_5.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+//The question defined out class name as Dataset but for GitHub better order I preferred E6_5
+public class E6_5 {
+    static double sum = 0;
+    static int count = 0;
+    static double min = Double.MAX_VALUE;
+    static double max = Double.MIN_VALUE;
+
+    public static void main(String[] args) {
+        System.out.print("please input floating point numbers:");
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextDouble()) {
+            double doubleScanned = in.nextDouble();
+            add(doubleScanned);
+            min(doubleScanned);
+            max(doubleScanned);
+        }
+        System.out.println("Details: ");
+        System.out.printf("Average of Values: %.2f \n", getAverage());
+        System.out.println("The Smallest of Values: " + min);
+        System.out.println("The Largest of Values: " + max);
+        System.out.printf("The Range of Values:%.2f \n", getRange());
+
+    }
+
+    static void add(double dNum) {
+        sum += dNum;
+        count++;
+    }
+
+    static void max(double dNumber) {
+        if (max < dNumber) {
+            max = dNumber;
+        }
+    }
+
+    static void min(double doubleNum) {
+        if (min > doubleNum) {
+            min = doubleNum;
+        }
+    }
+
+    static double getAverage() {
+        return sum / count;
+    }
+
+    static double getRange() {
+        return max - min;
+    }
+}
+
+
