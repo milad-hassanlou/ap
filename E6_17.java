1132e96ac8becb128994993e148f4c05774a8641
MiladHassanlou <miladhassanlou1@gmail.com>
2025-03-07 10:46:03 +0330

    Practice E6.17 Displaying a filled and hollow square.

---
+++ E6_17.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class E6_17 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please input a number:");
+        int num = in.nextInt();
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < 2*num+1; j++) {
+                if (j==num||((i!=0 && i!=num-1) && (num+1<j && j<2*num))){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
