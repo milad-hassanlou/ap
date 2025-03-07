e3c9d46c5d4330d874150f025d2e51998d953edc
MiladHassanlou <miladhassanlou1@gmail.com>
2025-03-07 09:54:31 +0330

    Practice E6.13 Change a decimal number to binary form.

---
+++ E6_13.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class E6_13 {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.print("Please input your number:");
+        int num=in.nextInt();
+        System.out.println("Binary form of the number:");
+        while(num>0){
+            System.out.println(num%2);
+            num/=2;
+        }
+    }
+}
