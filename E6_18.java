1455dedb229a8bd81b2c97e69462a34c1a9a9bad
MiladHassanlou <miladhassanlou1@gmail.com>
2025-03-07 11:28:41 +0330

    Practice E6.18 Displaying a filled diamond with (*).

---
+++ E6_18.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class E6_18 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please input a number:");
+        int n= in.nextInt();
+        for (int i=0;i<n;i++){
+            for(int j=0;j<2*n-1;j++) {
+                if (n-1-i<=j && j<=n-1+i){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i <n-1; i++) {
+            for (int j = 0; j < 2*n-1;j++) {
+                if(i<j && j<2*n-2-i){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
