973db902ceb10c1cb68db1b2c34277c4a589ba69
Milad <miladhassanlou100@gmail.com>
2025-03-24 12:53:51 +0330

    EX2_PM_1_2

---
+++ ap/exercises/ex2/Main_EX2_PM_1_2.java
@@ -0,0 +1,28 @@
+package ap.exercises.ex2;
+
+import java.util.Scanner;
+
+public class Main_EX2_PM_1_2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please input an integer number for the size of game-field:");
+        int k = in.nextInt();
+
+        char[][] arr = new char[k + 2][k + 2];
+        for (int i = 0; i < k + 2; i++) {
+            for (int j = 0; j < k + 2; j++) {
+                if (i == 0 || j == 0 || i == k + 1 || j == k + 1) {
+                    arr[i][j] = '*';
+                } else {
+                    arr[i][j] = ' ';
+                }
+            }
+        }
+        for (int i = 0; i < k + 2; i++) {
+            for (int j = 0; j < k + 2; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
