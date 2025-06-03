3e1f387381c65086a0d3f5f9276d22efdf923ad9
MiladHassanlou <miladhassanlou1@gmail.com>
2025-06-03 23:39:27 +0330

    EX6_SC1

---
+++ ap/exercises/ex6/Main_EX6_SC1.java
@@ -0,0 +1,63 @@
+package ap.exercises.ex6;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Main_EX6_SC1 {
+    // It's Better to Determine FilePath in Conf Class (Due to Commit Pushing It's Here)
+    private final String IMAGES_FILE_PATH = "E:/Codings Related Files/Rah/ImagesUrls/ImagesUrls.txt";
+
+    public void fillImagesFile() {
+        ArrayList<String> filePaths = new ArrayList<>();
+        for (int i = 1; i <= 224; i++) {
+            filePaths.add("E:/Codings Related Files/Rah/fetched_html" + i + ".html");
+        }
+        Set<String> imagesAddress = filePaths.stream()
+                .map(s -> {
+                    try {
+                        return Files.lines(Paths.get(s))
+                                .collect(Collectors.toList());
+                    } catch (IOException e) {
+                        return null;
+                    }
+                })
+                .filter(s -> s != null)
+                .flatMap(s -> s.stream())
+                .map(s -> getFirstImageUrl(s))
+                .filter(s -> s != null)
+                .filter(s -> s.length() > 1)
+                .collect(Collectors.toSet());
+
+        try {
+            PrintWriter out = new PrintWriter(IMAGES_FILE_PATH);
+            for (String img : imagesAddress) {
+                out.println(img);
+            }
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getFirstImageUrl(String htmlLine) {
+        String imageUrl = null;
+        int startIndex = htmlLine.indexOf("src=\"");
+        if (startIndex >= 0) {
+            try {
+                int srcLength = "src=\"".length();
+                int endIndex = htmlLine.indexOf("\"", startIndex + srcLength + 1);
+                imageUrl = htmlLine.substring(startIndex + srcLength + 1, endIndex);
+            } catch (Exception e) {
+
+            }
+        }
+        return imageUrl;
+    }
+}
+
